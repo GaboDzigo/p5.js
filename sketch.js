7631eb9b70166bbf85ea3be29b01e0142ea35ba2
GaboDzigo <gabodzigo@gmail.com>
2023-12-25 14:16:40 +0200

    js p5.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,16 @@
-// Code example
-// by Daniel Shiffman
-// The coding train
-// http://thecodingtrain.com
-// this code is based on everything i learned from
-// Lauren McCarthy and Cassie Tarakajan (creator of)
-
+// Flow / variables
+// code ! programming withg p5.js
 function setup() {
   createCanvas(400, 300);
+  background(0);
 }
 
 function draw() {
-  background(100);
-
-  rectMode(CENTER);
-
-  // this is the green rectangle in the center
-  fill(0, 255, 0);
-  stroke(0, 0, 255);
-  strokeWeight(4);
-  rect(200, 150, 150, 150);
-
-  // red circle
-  // later i want to make this circle animate
-  fill(255, 0, 0, 175);
   noStroke();
-  ellipse(150, 250, 100, 75);
+  fill(255);
+  circle(mouseX, mouseY, 24);
+}
+
+function mousePressed() {
+  background(0);
 }
