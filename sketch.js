c55bc33d813e7a2b681e0b0634fcb762de30073a
GaboDzigo <gabodzigo@gmail.com>
2023-12-26 17:15:40 +0200

    js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,13 @@
 // Flow / variables
 // code ! programming withg p5.js
-let squareSize;
-let lineWidth;
-
+var col = 0;
 function setup() {
   createCanvas(400, 300);
-  background(100);
 }
 
 function draw() {
-  lineWidth = random(4, 28);
-  squareSize = random(5, 250);
-
-  rectMode(CENTER);
-  strokeWeight(lineWidth);
-  stroke(0, 0, 255, 10);
-  fill(0, 255, 0, 10);
-  square(200, 150, squareSize);
+  col = mouseX / 2;
+  background(col);
+  fill(250, 118, 222);
+  ellipse(mouseX, 200, 64, 64);
 }
