fa76aa5c6ec6213f7bbfc54d9b778eacab345f09
GaboDzigo <gabodzigo@gmail.com>
2023-12-29 13:28:19 +0200

    js p5.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
-let speed = 3;
-let x = 0;
+var on = false;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(0);
+  if (on) {
+    background(0, 255, 0);
+  } else {
+    background(0);
+  }
+
   stroke(255);
   strokeWeight(4);
   noFill();
-  ellipse(x, 200, 100, 100);
-  if (x > width || x < 0) {
-    speed = speed * -1;
+  if (mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+    if (mouseIsPressed) {
+      background(0, 255, 0);
+    }
+    fill(255, 0, 200);
   }
-  x += speed;
+  rectMode(CENTER);
+  rect(300, 200, 100, 100);
 }
+function mousePressed() {
+  if (mouseX > 250 && mouseX < 350 && mouseY > 150 && mouseY < 250) {
+    on = !on;
+  }
+}
+
+//
+//   if (mouseIsPressed) {
+//     background(0, 255, 0);
+//   }
+//   fill(255, 0, 200);
+// }
