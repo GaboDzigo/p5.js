61d903005ed213b90ee8394b05233bbd95abb406
GaboDzigo <gabodzigo@gmail.com>
2023-12-31 16:29:04 +0200

    js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(955, 900);
 }
 
 function draw() {
@@ -7,15 +8,10 @@ function draw() {
   strokeWeight(4);
   stroke(255);
 
-  var x  = 0;
-  while(x <= width) {
-    fill(0, 200, 255)
-  ellipse(x, 100, 25, 25);
-  x += 50;
-  }
-
   for(var x = 0; x <= width; x += 50) {
-    fill(255, 0 , 200)
-    ellipse(x, 300, 25, 25);
+    for(var y = 0; y <= width; y += 50){
+    fill(random(255), 0, random(255))
+    ellipse(x, y, 25, 25);
+    }
   }
 }
