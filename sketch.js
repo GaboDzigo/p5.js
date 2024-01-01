f4636d77a1a2a85f67c7a0d3a378bbfa1c791791
GaboDzigo <gabodzigo@gmail.com>
2024-01-01 15:27:22 +0200

    p5.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,15 @@
 function setup() {
-  createCanvas(600, 400);
+  var km = milesTokm(26.3)
+  println(km);
+  var km2 = milesTokm(100);
+  println(km2)
 }
 
 function draw() {
-  background(50);
-  lolipop(100, 100, 50);
-  lolipop(300, 200, 150);
-  lolipop(200, 300, 100);
+
 }
 
-function lolipop(x, y, diameter) {
-
-  fill(0,200,255),
-  rect(x-10,y,20,150),
-
-  fill(255,0,200),
-  ellipse(x,y,diameter,diameter)
+function milesTokm(miles) {
+  var km = miles *  1.6
+  return km;
 }
