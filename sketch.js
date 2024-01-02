4db8e79791ce113e977a0e09c367b16375b29821
GaboDzigo <gabodzigo@gmail.com>
2024-01-02 13:28:38 +0200

    p5.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,47 @@
+let bubble1;
+let bubble2;
 function setup() {
-  var km = milesTokm(26.3)
-  println(km);
-  var km2 = milesTokm(100);
-  println(km2)
+  createCanvas(600,400);
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+  print(Bubble.x, Bubble.y)
 }
 
 function draw() {
-
+  background(0);
+  bubble1.move();
+  bubble1.show();
+  bubble2.move();
+  bubble2.show();
 }
 
-function milesTokm(miles) {
-  var km = miles *  1.6
-  return km;
-}
+class Bubble {
+  constructor() {
+    this.x = 200;
+    this.y = 150;
+  }
+
+  move() {
+    this.x += random(-5, 5);
+    this.y += random(-5, 5);
+  }
+
+  show() {
+    stroke(255);
+      strokeWeight(4)
+      noFill();
+       ellipse( this.x,  this.y, 24,24)
+    }
+  }
+
+
+// function show() {
+//   stroke(255);
+//   strokeWeight(4)
+//   noFill();
+//   ellipse(bubble.x, bubble.y, 24,24)
+// }
+
+// function move() {
+// 
+// }
