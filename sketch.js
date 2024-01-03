6149a4b00c9857ab7108d046c933e4c5e13a8998
GaboDzigo <gabodzigo@gmail.com>
2024-01-03 15:22:11 +0200

    js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,16 @@
-var words = ["rainbow","heart","purple","friendship"];
-var index = 0;
+ var nums = [100,25,46,72];
+
+var num = 23;
 function setup() {
-createCanvas(400,400)
+createCanvas(500,400)
 }
 
 function draw() {
   background(0);
-  fill(255);
-  textSize(32)
-  text(words[index],12,200)
-}
 
-function mousePressed() {
-  index = index + 1;
-  if(index == 4) {
-    index = 0;
+  for(var i = 0; i < 4; i++) {
+    stroke(255);
+    noFill(51);
+    ellipse(i *100 + 100,200, nums[i], nums[i]);
   }
 }
