efedb0e5ead5014b61147f15892f6fbe20d30429
GaboDzigo <gabodzigo@gmail.com>
2024-01-06 14:45:11 +0200

    js

--- sketch.js
+++ sketch.js
@@ -3,22 +3,17 @@ let bubbles = [];
 
 function setup() {
   createCanvas(600,400);
-  for(let i = 0; i < 15; i++) {
-  let x = random(width);
-  let y = random(height);
-  let r = random(10,50)
-  let b  = new Bubble(x, y , r);
-  bubbles.push(b)
-}
 }
 
-function mousePressed() {
-  for(let i = bubbles.length-1; i >= 0; i--) {
-    if(bubbles[i].contains(mouseX, mouseY)) {
-     bubbles.splice(i, 1)
-    }
+function mouseDragged() {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10,50)
+    let b  = new Bubble(mouseX, mouseY, r);
+    bubbles.push(b);
 }
-}
+
+
 function draw() {
   background(0);
   for(let i = 0; i < bubbles.length; i++) {
